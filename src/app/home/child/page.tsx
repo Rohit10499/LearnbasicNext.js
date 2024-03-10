27e8d07806527a86b7e9a6c60fd050865bc2bc2e
Rohit10499 <rohitmaurya10499@gmail.com>
2024-03-10 19:14:05 +0530

    basic layout and folder

---
+++ src/app/home/child/page.tsx
@@ -0,0 +1,6 @@
+export default function Subhome(){
+
+    return (<h2>
+        in side home folder an other folder
+    </h2>)
+}
---
+++ src/app/home/layout.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+  <>        
+   <h2>inner layout Item</h2>
+        {children}
+        </>
+
+  );
+}
