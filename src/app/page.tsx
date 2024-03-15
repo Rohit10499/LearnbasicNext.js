2c77b4780a3936c0dd915b34b0587e4efcaf5cee
Rohit10499 <rohitmaurya10499@gmail.com>
2024-03-15 17:42:29 +0530

    make navebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-     <h1 className="text-2xl text-center">learn next.JS</h1>
-  );
+<div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+<h1 className="text-2xl text-center">learn next.JS</h1>
+
+</div>  );
 }
