1b9ab15c144797d5ee4892f6a2c0c5b2d2516534
Rohit10499 <rohitmaurya10499@gmail.com>
2024-03-22 02:33:32 +0530

    made home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-<div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-<h1 className="text-2xl text-center">learn next.JS</h1>
-
-</div>  );
+    <div className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+    </div>
+  );
 }
